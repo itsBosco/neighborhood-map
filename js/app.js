ee257aaa89ed8c62676c46e452602bd85d54d01e
Bosco <zahiersmith96@gmail.com>
2016-08-06 18:16:21 -0400

    feat: Added flickr api

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 /*jshint loopfunc: true */
 var map, bounds, InfoWindow, locations;
+var flickr_api_key = 'c409285ba83e15950d72eddb6f85a5ec';
 var markers = [];
+//hardcoded locations
+var locations = [{
+    title: "Golden Gate Bridge",
+    position: {
+        lat: 37.8199,
+        lng: -122.4783
+    }
+}, {
+    title: "Union Square",
+    position: {
+        lat: 37.7880,
+        lng: -122.4074
+    }
+}, {
+    title: "Chinatown",
+    position: {
+        lat: 37.7941,
+        lng: -122.4078
+    }
+}, {
+    title: "AT&T Park",
+    position: {
+        lat: 37.7786,
+        lng: -122.3893
+    }
+}, {
+    title: "Cable Car System",
+    position: {
+        lat: 37.7907,
+        lng: -122.4188
+    }
+}, ];
 
 
 //VIEWMODEL
@@ -16,7 +49,6 @@ function AppViewModel(locations) {
     };
 
     self.filter = ko.observable("");
-
 
     //Takes items from the markers array and filters them based of the text in the filter
     self.filteredItems = ko.dependentObservable(function() {
@@ -43,7 +75,7 @@ function AppViewModel(locations) {
         }
 
     });
-}
+} //end of viewmodel
 
 //Init Google Map
 function initMap() {
@@ -52,44 +84,10 @@ function initMap() {
             lat: 37.7749,
             lng: -122.4194
         },
-        zoom: 12
+        zoom: 13
     });
     InfoWindow = new google.maps.InfoWindow();
     bounds = new google.maps.LatLngBounds();
-
-
-    //hardcoded locations
-    var locations = [{
-        title: "Golden Gate Bridge",
-        position: {
-            lat: 37.8199,
-            lng: -122.4783
-        }
-    }, {
-        title: "Union Square",
-        position: {
-            lat: 37.7880,
-            lng: -122.4074
-        }
-    }, {
-        title: "Chinatown",
-        position: {
-            lat: 37.7941,
-            lng: -122.4078
-        }
-    }, {
-        title: "AT&T Park",
-        position: {
-            lat: 37.7786,
-            lng: -122.3893
-        }
-    }, {
-        title: "Cable Car System",
-        position: {
-            lat: 37.7907,
-            lng: -122.4188
-        }
-    }, ];
 
     ko.applyBindings(new AppViewModel(locations));
 }
@@ -105,18 +103,49 @@ function addMarker(location) {
         title: location.title,
         animation: google.maps.Animation.DROP,
         populateInfoWindow: populateInfoWindow,
+        image: '',
+        //gets flikr image
+        getImage: function() {
+            var marker = this;
+            $.ajax({
+                url: ' https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' +
+                    flickr_api_key + '&per_page=50&radius=0.5&lat=' + location.position.lat +
+                    '&lon=' + location.position.lng + '&format=json&nojsoncallback=1',
+                dataType: 'json',
 
+                success: function jsonFlickrApi(response) {
+                    var photoData = response.photos.photo[2];
+                    var farm = photoData.farm;
+                    var photoID = photoData.id;
+                    var secretID = photoData.secret;
+                    var server = photoData.server;
+                    //sets marker image as the results from the flickr api
+                    setImage(marker, '<img src=https://farm' + farm + '.staticflickr.com/' +
+                        server + '/' + photoID + '_' + secretID + '.jpg');
+                },
+
+                error: function(e) {
+                    setImage(marker, '<h1>Failed to get image</h1>');
+                }
+            });
+
+        },
+        //changes marker to bouncing when it is clicked
         toggleBounce: function() {
+            //makes sure that more than one marker is bouncing at a time
+            markers.forEach(function(marker) {
+                marker.setAnimation(null);
+            });
+            //checks if current marker is already bouncing
             if (this.getAnimation() !== null) {
                 this.setAnimation(null);
             } else {
                 this.setAnimation(google.maps.Animation.BOUNCE);
             }
-        }
-
+        },
     });
+    marker.getImage();
     marker.addListener('click', function() {
-        //populateInfoWindow(this, InfoWindow);
         this.toggleBounce();
         this.populateInfoWindow(this, InfoWindow);
     });
@@ -147,19 +176,10 @@ function deleteMarkers() {
     markers = [];
 }
 
-//bounds.extend(markers[i].position);
-
 //Infowindow for markers
 function populateInfoWindow(marker, infowindow) {
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-}
-
-//Infowindow for markers
-function populateInfoWindow(marker, infowindow) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.setContent('<div class="infowindow">' + '<h1>' + marker.title + '</h1>' + '<br>' + marker.image + '<br>' + '<p>Recent flickr upload of ' + marker.title + '</p>' + '</div>');
     infowindow.open(map, marker);
     //stops bouncing when user closes infowindow
     infowindow.addListener('closeclick', function() {
@@ -169,7 +189,14 @@ function populateInfoWindow(marker, infowindow) {
 
 //Handles map not loading error
 function mapError() {
+    'use strict';
     alert('Failed to load map');
+}
+
+//sets the given markers image to the image url from the flickr api
+function setImage(marker, image) {
+    marker.image = image;
+    return image;
 }
 
 //Checks if a certain string starts with a certain character(had to add this here for it to work)
